d966aff6b5821fd76c93ffdc352fc3e5ae9b0363
Cody Dempsey <cdempsey1@stu.jsu.edu>
2017-07-03 01:17:15 -0500

    finishing the WarDeck class

---
+++ src/test/java/War/WarDeckTest.java
@@ -0,0 +1,38 @@
+package War;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class WarDeckTest{
+	
+	private WarDeck wd;
+	
+	@Before
+	public void setUp(){
+		wd = new WarDeck();
+	}
+	
+	private int[] testDeck = new int [51];
+	
+	@Test
+	public void testThatFirstCardIsAOne(){
+		assertEquals(wd.getDeck().get(0), 1.0, 1.000000);
+	}
+	
+	@Test
+	public void testThatLastCardisAThirteen(){
+		assertEquals(wd.getDeck().get(51),13.0, 1.00000);
+	}
+	
+	@Test
+	public void testThatPlayerHandIsDealt(){
+		assertEquals(wd.getPlayerHand().size(), 26);
+	}
+	
+	@Test
+	public void testThatEnemyHandIsDealt(){
+		assertEquals(wd.getEnemyHand().size(), 26);
+	}
+	
+	
+}
